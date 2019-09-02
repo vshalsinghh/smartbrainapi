021a0c29717a8efd933f6a90b7573cb103bebc74
unknown <vishalsingh2402@gmail.com>
2019-09-02 17:20:08 +0530

    added ssl

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-polished-94952,
+    host : process.env.DATABASE_URL,
      //connectionString: process.env.DATABASE_URL,
   ssl: true
   }
