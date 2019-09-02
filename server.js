bf0b0d308a3f7dbe8da009d237e5f7c612698dc3
unknown <vishalsingh2402@gmail.com>
2019-09-02 17:16:54 +0530

    added ssl

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smartbrain'
+    host : 'postgresql-polished-94952,
+     //connectionString: process.env.DATABASE_URL,
+  ssl: true
   }
 });
 
