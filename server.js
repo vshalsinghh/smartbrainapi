36aa97142a87bcf5fc6ae6367dcef9a9c93282bf
unknown <vishalsingh2402@gmail.com>
2019-09-02 12:19:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors= require('cors');
+const knex = require('knex')
+
+
+const signin = require('./controllers/signin');
+const register =require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+
+
+const app = express();
+
+ app.use(bodyParser.json())
+ app.use(cors())
+
+
+app.get('/', (req,res) => {
+	res.send(database.users)
+})
+
+app.post('/signin',signin.handleSignin(db, bcrypt))
+//register
+app.post('/register',(req,res) => {register.handleRegister(req,res, db, bcrypt)})
+//get profile
+app.get('/profile/:id', (req,res) => {profile.handleProfile(req,res,db)})
+//update entries
+app.put('/image', (req,res) => {image.handleImage(req,res,db)} )
+
+
+app.listen(3000, () => {
+	console.log("its working")
+})
+
+/*
+
+-->res =this is working
+/signin -->post = success/fail
+/register --> post = user
+/profile/userid--> get =user
+/image--> put = user
+
+*/
